da194c3d48442e315e36ae7fab3c39a63c47e59e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-20 15:16:11 +0800

    Spoof: Add preference for displaying current PIF properties
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/arrow/support/fragments/MiscSettings.java
+++ src/com/arrow/support/fragments/MiscSettings.java
@@ -19,6 +19,7 @@ package com.arrow.support.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
@@ -143,6 +144,14 @@ public class MiscSettings extends SettingsPreferenceFragment implements OnPrefer
             updatePropertiesFromUrl("https://raw.githubusercontent.com/chiteroman/PlayIntegrityFix/main/module/pif.json");
             return true;
         });
+        
+        Preference showPropertiesPref = findPreference("show_pif_properties");
+        if (showPropertiesPref != null) {
+            showPropertiesPref.setOnPreferenceClickListener(preference -> {
+                showPropertiesDialog();
+                return true;
+            });
+        }
     }
 
     private void openFileSelector(int requestCode) {
@@ -164,6 +173,40 @@ public class MiscSettings extends SettingsPreferenceFragment implements OnPrefer
                 }
             }
         }
+    }
+    
+    private void showPropertiesDialog() {
+        StringBuilder properties = new StringBuilder();
+        try {
+            JSONObject jsonObject = new JSONObject();
+            String[] keys = {
+                "persist.sys.pihooks_ID",
+                "persist.sys.pihooks_BRAND",
+                "persist.sys.pihooks_DEVICE",
+                "persist.sys.pihooks_FINGERPRINT",
+                "persist.sys.pihooks_MANUFACTURER",
+                "persist.sys.pihooks_MODEL",
+                "persist.sys.pihooks_PRODUCT",
+                "persist.sys.pihooks_SECURITY_PATCH",
+                "persist.sys.pihooks_DEVICE_INITIAL_SDK_INT"
+            };
+            for (String key : keys) {
+                String value = SystemProperties.get(key, null);
+                if (value != null) {
+                    String buildKey = key.replace("persist.sys.pihooks_", "");
+                    jsonObject.put(buildKey, value);
+                }
+            }
+            properties.append(jsonObject.toString(4));
+        } catch (JSONException e) {
+            Log.e(TAG, "Error creating JSON from properties", e);
+            properties.append(getString(R.string.error_loading_properties));
+        }
+        new AlertDialog.Builder(getContext())
+            .setTitle(R.string.show_pif_properties_title)
+            .setMessage(properties.toString())
+            .setPositiveButton(android.R.string.ok, null)
+            .show();
     }
 
     private void updatePropertiesFromUrl(String urlString) {
